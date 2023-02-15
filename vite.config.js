4676bf9da480c7d0db08279587495236d9bb99d4
OleksiiVTS <119164270+OleksiiVTS@users.noreply.github.com>
2023-02-15 18:56:37 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import injectHTML from 'vite-plugin-html-inject';
 
 export default defineConfig({
   root: 'src',
   build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        nested: resolve(__dirname, 'src/restaurant.html'),
+        nested: resolve(__dirname, 'src/hotel.html'),
+        nested: resolve(__dirname, 'src/room2lx.html'),
+        nested: resolve(__dirname, 'src/room2.html'),
+        nested: resolve(__dirname, 'src/room3.html'),
+      },
+    },
     outDir: '../dist',
   },
   plugins: [injectHTML()],
