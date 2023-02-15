8a292d584605a66c6caf1d27e1ba84218db51b2d
SSSerg05 <ssserg05@gmail.com>
2023-02-16 00:02:40 +0200

    play slider
    Signed-off-by: SSSerg05 <ssserg05@gmail.com>

--- src/js/room-hero-wrap.js
+++ src/js/room-hero-wrap.js
@@ -1,4 +1,4 @@
-import Swiper from "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js";
+//import Swiper from "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js";
 
 const swiper = new Swiper('.swiper', {
   // Optional parameters
