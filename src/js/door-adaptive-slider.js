a28c03c4dda5c761cfa4e27fc50825db85729954
SSSerg05 <ssserg05@gmail.com>
2023-02-18 20:21:34 +0200

    js
    Signed-off-by: SSSerg05 <ssserg05@gmail.com>

--- src/js/door-adaptive-slider.js
+++ src/js/door-adaptive-slider.js
@@ -12,25 +12,25 @@ function doorSlider() {
     function init() {
         // console.log('resize');
         let lenSprite = document.querySelector('.door-slider').offsetWidth;
-        console.log('lenSprite:', lenSprite);
-
         let totalWidth = 0;
         let gaps = { 374: 20, 662: 34, 875: 87 };
+
+        // масив розміру картинок {0:270, 1:162, ....}
         arr = {};
+
+        // 
         let aImageSize = {162 : 34, 270 : 34, 274 : 20};
         let gap = 20;
         gap = gaps[lenSprite] ? gaps[lenSprite] : gap; 
-        
-        console.log('gap:', gap);
+
         images.forEach((element, key) => {
+
             // зміна розміру картинки під слайдер 
             for ( let item in aImageSize ) {
                 if ((element.clientWidth <= item) && (aImageSize.item == gap)) {
                     element.width = item + 'px';
                     element.heidht = sliderLine.offsetHeight + 'px';
                 }
-
-                console.log('e.width:', element.width);
             }
 
             arr[key] = element.width + gap;
@@ -39,11 +39,7 @@ function doorSlider() {
          })
 
         width = arr[count];
-        console.log('arr', arr);
-        console.log('width', width);
-
         sliderLine.style.width = totalWidth + 'px';
-        console.log('sliderLine-0:', sliderLine.style.width);
         
         rollSlider();
     }
@@ -51,6 +47,7 @@ function doorSlider() {
     window.addEventListener('resize', init);
     init();
 
+    
     keyNext.addEventListener('click', () => {
         count++;
         if ( count >= images.length) {
@@ -70,17 +67,14 @@ function doorSlider() {
     });
 
     function rollSlider() {
-        console.log('sliderLine-1:', sliderLine.style.width, arr[count]);
-
         let currWidth = 0;
+
         for (let item in arr) {
             if (item >= count) break;
             currWidth += arr[item];
         }
-        console.log('currWidth:',  currWidth);
-        sliderLine.style.transform = 'translate(-' + currWidth + 'px)';
 
-        console.log('sliderLine-2:', sliderLine.style.width, arr[count]);
+        sliderLine.style.transform = 'translate(-' + currWidth + 'px)';
         // sliderLine.style.transform = 'translate(-' + count * width + 'px)';
     }
 }
