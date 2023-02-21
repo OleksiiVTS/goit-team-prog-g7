77a8c409d85206721c73132b403224671c746f58
OleksiiVTS <119164270+OleksiiVTS@users.noreply.github.com>
2023-02-21 23:37:18 +0200

    add buttonup

--- src/js/buttonup.js
+++ src/js/buttonup.js
@@ -1,13 +1,18 @@
- 
 window.addEventListener('scroll',function(){
     const scroll = document.querySelector('.button-up');
     scroll.classList.toggle("button-up-show", window.scrollY>300)
 })
-    function scrollTopTop(){
-      window.scrollTo({
-        top:0,
-      })
-    }
+    
+// Функція, що прокручує сторінку до верхньої частини
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// Додаємо обробник події на кнопку для підняття вгору
+scrollToTopBtn.addEventListener("click", scrollToTop);
+    
+
 window.addEventListener('scroll',function(){
     const scroll = document.querySelector('.arrow-up');
     scroll.classList.toggle("button-up-show", window.scrollY>300)
