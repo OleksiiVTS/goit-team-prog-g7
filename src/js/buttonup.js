660cf977da877405f2a1f1c8ba949bebbb050576
OleksiiVTS <119164270+OleksiiVTS@users.noreply.github.com>
2023-02-21 23:01:14 +0200

    add

--- src/js/buttonup.js
+++ src/js/buttonup.js
@@ -1,6 +1,6 @@
  
 window.addEventListener('scroll',function(){
-    var scroll = document.querySelector('.button-up');
+    const scroll = document.querySelector('.button-up');
     scroll.classList.toggle("button-up-show", window.scrollY>300)
 })
     function scrollTopTop(){
@@ -9,31 +9,6 @@ window.addEventListener('scroll',function(){
       })
     }
 window.addEventListener('scroll',function(){
-    var scroll = document.querySelector('.arrow-up');
+    const scroll = document.querySelector('.arrow-up');
     scroll.classList.toggle("button-up-show", window.scrollY>300)
   })
-
-    
-  // (() => {
-  //   const refs = {
-  //     openModalBtn: document.querySelector("[data-modal-open]"),
-  //     closeModalBtn: document.querySelector("[data-modal-close]"),
-  //     modal: document.querySelector("[data-modal]"),
-  //   };
-  
-  //   refs.openModalBtn.addEventListener("click", toggleModal);
-  //   refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-  //   function toggleModal() {
-  //     refs.modal.classList.toggle("is-hidden");
-  //   }
-  // })();
-
-  
-   
-
-//   $("#openmodal").on("change", function () {        
-//     if($(this).val() === 'openmodaloption'){
-//        $('#modal').modal('show');
-//    }
-// });
