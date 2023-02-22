e61db0eacae1814a26f9344a03298981080ae747
OleksiiVTS <119164270+OleksiiVTS@users.noreply.github.com>
2023-02-22 15:48:26 +0000

    Deploying to gh-pages from @ OleksiiVTS/goit-team-prog-g7@10998a3c10542fbc393d2c2fbbf7acee3c9cf864 🚀

---
+++ assets/modal-res-41630978.js
@@ -0,0 +1,1 @@
+const e=document.getElementById("inpt1"),t=document.getElementById("res1");e.addEventListener("keyup",function(){t.value=e.value*1e3});
--- assets/modal-res-80b9f781.js
+++
@@ -1,1 +0,0 @@
-const e=document.getElementById("inpt"),t=document.getElementById("res");e.addEventListener("keyup",function(){t.value=e.value*1e3});
