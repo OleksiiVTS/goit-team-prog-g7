10998a3c10542fbc393d2c2fbbf7acee3c9cf864
OleksiiVTS <119164270+OleksiiVTS@users.noreply.github.com>
2023-02-22 17:48:06 +0200

    виправленя модального вікна Влад

--- src/js/modal-res.js
+++ src/js/modal-res.js
@@ -1,5 +1,5 @@
-const input = document.getElementById("inpt");
-    const inputResult = document.getElementById("res");
+const input = document.getElementById("inpt1");
+    const inputResult = document.getElementById("res1");
 
     input.addEventListener("keyup", function () {
       inputResult.value = input.value * 1000;
